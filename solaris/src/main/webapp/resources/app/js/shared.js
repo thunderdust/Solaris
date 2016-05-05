e49d7b64bbf743e3ad6c5d696bfeaf10dc3ed2a5
Liu Weiran <liu_w@worksap.co.jp>
2016-05-05 20:34:36 +0800

    Created sticky footer with auto hiding; Add contents to IT Training

--- solaris/src/main/webapp/resources/app/js/shared.js
+++ solaris/src/main/webapp/resources/app/js/shared.js
@@ -1,4 +1,5 @@
 var Solaris = {};
+var footerStatus = 'shown';
 
 jQuery.fn.serializeObject = function() {
 	var arrayData, objectData;
@@ -32,3 +33,66 @@ $.extend( $.fn.dataTable.Responsive, {
     searching: false,
     ordering:  false
 } );
+
+/* Full opacity */
+function showFooter() {
+    // show footer if it is NOT already shown
+	if (footerStatus!=='shown'){
+		adjustFooterOpacity('fast', 1);
+		footerStatus = 'shown';
+	}
+	$(window).bind('scroll', checkScroll);
+};
+
+/* Low opacity */
+function hideFooter() {
+	
+	// hide footer if it is NOT already hidden
+	if (footerStatus!=='hide'){
+		adjustFooterOpacity('fast', 0.2);
+		footerStatus = 'hide';
+	}
+	$(window).bind('scroll', checkScroll);
+};
+
+function adjustFooterOpacity(duration, opacity){
+	// execute when footer exists
+	if (!$.isEmptyObject($('.solaris-footer').eq(0))){
+			$('.solaris-footer').fadeTo(duration, opacity);
+	}
+}
+
+function checkScroll(){
+	
+	// Scroll to the page bottom 
+	if ($(window).scrollTop() + $(window).height() > $(document).height() - 100){	
+		$(window).unbind('scroll');
+		showFooter();
+	}
+	else {
+		$(window).unbind('scroll');
+		hideFooter();
+	}
+};
+
+function updateFooterByDocHeight(){
+
+    // Fade footer ONLY when content is longer than window  
+    if (document.documentElement.scrollHeight > document.documentElement.clientHeight){
+        console.log('scrollable');
+        adjustFooterOpacity('fast', 0.2);
+        footerStatus = 'hidden';        
+    }
+    else {
+        console.log('NOT scrollable');
+        footerStatus = 'shown'; 
+    }
+    $(window).scroll(checkScroll);
+}
+
+
+$(document).ready(function() {
+    // wait for data table loading 
+	setTimeout(updateFooterByDocHeight(), 800);
+});
+		
--- solaris/src/main/webapp/resources/app/js/sub-trainings.js
+++ solaris/src/main/webapp/resources/app/js/sub-trainings.js
@@ -1,5 +1,8 @@
 var currentPageIndex;
 var paragraphList;
+/******** Variables for footer handling migrated from shared.js ***********/
+var footerStatus = 'shown';
+/******** Variables for footer handling migrated from shared.js ***********/
 
 $(document).ready(function() {
     var initPage = function() {
@@ -10,6 +13,10 @@ $(document).ready(function() {
     }
 
     initPage();
+
+    // wait for data table loading 
+    setTimeout(updateFooterByDocHeight(), 800);
+
 });
 
 function hideAll(){
@@ -29,6 +36,7 @@ function show(index) {
     $('.solaris-paragraph').eq(index).fadeIn();
     currentPageIndex = index;
     //console.log('Showing '+ index);
+    updateFooterByDocHeight();
 }
 
 function goPrevPage() {
@@ -48,3 +56,67 @@ function goNextPage() {
 function hideMenu(){
 	$('#chapter-modal').modal('hide');
 }
+
+
+
+/******** Functions for footer handling migrated from shared.js ***********/
+
+
+function showFooter() {
+    // show footer if it is NOT already shown
+    if (footerStatus!=='shown'){
+        adjustFooterOpacity('fast', 1);
+        footerStatus = 'shown';
+    }
+    $(window).bind('scroll', checkScroll);
+};
+
+
+function hideFooter() {
+    
+    // hide footer if it is NOT already hidden
+    if (footerStatus!=='hide'){
+        adjustFooterOpacity('fast', 0.2);
+        footerStatus = 'hide';
+    }
+    $(window).bind('scroll', checkScroll);
+};
+
+function adjustFooterOpacity(duration, opacity){
+    // execute when footer exists
+    if (!$.isEmptyObject($('.solaris-footer').eq(0))){
+            $('.solaris-footer').fadeTo(duration, opacity);
+    }
+}
+
+function checkScroll(){
+    
+    // Scroll to the page bottom 
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100){   
+        $(window).unbind('scroll');
+        showFooter();
+    }
+    else {
+        $(window).unbind('scroll');
+        hideFooter();
+    }
+};
+
+function updateFooterByDocHeight(){
+
+    console.log("");
+    // Fade footer ONLY when content is longer than window  
+    if (document.documentElement.scrollHeight > document.documentElement.clientHeight){
+        console.log('scrollable');
+        adjustFooterOpacity('fast', 0.2);
+        footerStatus = 'hidden';        
+    }
+    else {
+        console.log('NOT scrollable');
+        footerStatus = 'shown'; 
+    }
+    $(window).scroll(checkScroll);
+}
+
+
+/******** Variables for footer handling migrated from shared.js ***********/
