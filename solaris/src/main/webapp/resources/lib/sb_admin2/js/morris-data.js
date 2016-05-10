70e4ee3dcef50756bef4f727ab87c2c1e38c2ce6
Liu Weiran <liu_w@worksap.co.jp>
2016-05-10 19:20:08 +0800

    Customized business growth chart in dashboard

--- solaris/src/main/webapp/resources/lib/sb_admin2/js/morris-data.js
+++ solaris/src/main/webapp/resources/lib/sb_admin2/js/morris-data.js
@@ -2,60 +2,36 @@ $(function() {
 
     Morris.Area({
         element: 'morris-area-chart',
-        data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+        data: [
+        {
+            period: '2015 Q2',
+            Deals: 4,
+            Laptop: 5,
+            Customer: 4
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2015 Q3',
+            Deals: 10,
+            Laptop: 14,
+            Customer: 10
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2015 Q4',
+            Deals: 11,
+            Laptop: 16,
+            Customer: 12
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2016 Q1',
+            Deals: 12,
+            Laptop: 18,
+            Customer: 16
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2016 Q2',
+            Deals: 15,
+            Laptop: 22,
+            Customer: 20
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['A', 'B', 'C'],
+        ykeys: ['Deals', 'Laptop', 'Customer'],
+        labels: ['Deals', 'Laptop Sold', 'Customer Registered'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
